78b51338326b1bd13c1da08353ef62bf0fd9bc2b
BlankFox <caq201902@outlook.com>
2022-08-01 21:20:47 +0800

    init

---
+++ docs/database.js
@@ -0,0 +1,25 @@
+const blog=[
+    {
+        picUrl: 'test',
+        date: '2020-7-31',
+        title: '测试标题1',
+        describe: '测试内容1',
+        link: '测试连接1'
+    },{
+        picUrl: 'test',
+        date: '2020-7-31',
+        title: '测试标题1',
+        content: '测试内容1',
+        link: '测试连接1'
+    },{
+        picUrl: 'test',
+        date: '2020-7-31',
+        title: '测试标题1',
+        content: '测试内容1',
+        link: '测试连接1'
+    }
+]
+
+export function _getBlogList(){
+    return blog
+}
